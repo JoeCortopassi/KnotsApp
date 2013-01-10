2bea5ff7f13c0f6e2e5d92b07e77edb3b01df875
Joe Cortopassi <joe@joecortopassi.com>
2013-01-09 18:40:00 -0800

    Initial Commit

---
+++ KnotsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KnotsApp
+//
+//  Created by Joe Cortopassi on 1/9/13.
+//  Copyright (c) 2013 Joe Cortopassi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
